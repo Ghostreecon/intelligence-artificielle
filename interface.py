3c8033c1b91e5cb28c1c9b9fcdb601e050bf4e84
Ghostreecon <86111496+Ghostreecon@users.noreply.github.com>
2023-12-12 07:46:49 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,6 +1,5 @@
-# -*- coding: utf8 -*-
-
 import random
+import time
 import json
 import pickle
 import numpy as np
@@ -8,7 +7,8 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 import tkinter as tk
-from tkinter import scrolledtext, font
+from tkinter import scrolledtext
+from tkinter import font
 from PIL import Image, ImageTk
 
 lemmatizer = WordNetLemmatizer()
@@ -65,17 +65,24 @@ def get_response(intents_list, intents_json):
     return result
 
 
+
 # Initialisation des messages
 messages = []
+def typing_indicator():
+    chat_history.insert(tk.END, 'Conseiller est en train d\'écrire...')
+    chat_history.see(tk.END)
+    time.sleep(0.5)
+    chat_history.delete("end-1l", tk.END)
 
 
 def send_message():
     # Récupération du message saisi
     chat_history.config(state=tk.NORMAL)
     message = user_input.get()
-
     chat_history.insert(tk.END, 'Vous: ' + message + '\n\n')
-    chat_history.config(state=tk.DISABLED)
+
+    # Affichage du typing indicator pendant 0.5s
+    chat_history.after(0, typing_indicator)
 
     # Prédiction de la réponse du chatbot
     ints = predict_class(message)
@@ -85,10 +92,8 @@ def send_message():
     messages.append(('bot', res))
 
     # Affichage de la réponse du chatbot
-    chat_history.config(state=tk.NORMAL)
     chat_history.insert(tk.END, 'Conseiller: '+ res + '\n\n', 'bot_message')
     chat_history.config(state=tk.DISABLED)
-
     # Effacer le champ de saisie
     user_input.delete(0, tk.END)
 
@@ -96,11 +101,30 @@ def send_message():
 # Création de la fenêtre principale
 root = tk.Tk()
 root.title("Chatbot")
-root.configure(bg="#a0a2cb")
-
-# Ajout d'une icône à la fenêtre
+font = font.Font(family="Helvetica", size=12)
+bg_color = "#a0a2cb"
 app_icon = Image.open('images/chat_ca.png')
 app_icon = ImageTk.PhotoImage(app_icon)
+
 root.iconphoto(False, app_icon)
 
 # Création du widget Text pour afficher l'historique des messages
+chat_history = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=20, font=("Helvetica", 12))
+chat_history.tag_config('bot_message', justify='right')
+chat_history.tag_config('bot_message', justify='right')
+chat_history.pack(side=tk.TOP, fill=tk.BOTH, padx=5, pady=5)
+
+# Créer un cadre pour la zone de saisie du chat de l'utilisateur 1
+user1_input_frame = tk.Frame(root, bg=bg_color)
+user1_input_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
+
+# Créer une entrée pour la saisie de texte de l'utilisateur 1
+user_input = tk.Entry(user1_input_frame, font=font, bg="white", fg="black", relief="flat",borderwidth=2, highlightthickness=0)
+user_input.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=5, pady=5)
+
+# Créer un bouton pour envoyer le message de l'utilisateur 1
+user1_send_button = tk.Button(user1_input_frame, text="Envoyer", font="helvetica 10 bold", bg="#00af9c", fg="white", activebackground="#00af9c", activeforeground="white", relief="flat", borderwidth=0, highlightthickness=0, command=send_message)
+user1_send_button.pack(side=tk.RIGHT, padx=5, pady=5)
+
+
+root.mainloop()
